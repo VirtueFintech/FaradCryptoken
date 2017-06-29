2d629f37d86a04ffe4f052f62a411d216d619482
Da Few <1ofdafew@gmail.com>
2017-06-30 01:23:47 +0800

    Fixed the transfer, and transferFrom

--- test/test-token.js
+++ test/test-token.js
@@ -2,15 +2,21 @@
 var Token = artifacts.require('./Token.sol');
 
 contract('Token', function(accounts) {
+  var supply = 125000000; // 125m
+  var owner = accounts[0];
+  var dev1 = accounts[1];
 
-  it('shall put 100m for creator', function() {
-    var expected = 100000000; // 100m
-    var owner = accounts[0];
+  it('shall put 125m for creator', function() {
+    var token;
 
-    return Token.new('Farad', 'FRD', 8, expected).then(function(instance) {
-      return instance.balanceOf.call(owner);
+    return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
+      token = instance;
+      return token.balanceOf.call(owner);
     }).then(function(balance) {
-      assert.equal(balance.valueOf(), expected, "100m wasn't in the first account");
+      assert.equal(balance.toNumber(), supply, "125m wasn't in the first account");
+      return token.name.call();
+    }).then(function(name) {
+      console.log('name:', name);
     });
   });
 
@@ -18,50 +24,45 @@ contract('Token', function(accounts) {
     // transfer 1000 to addr1
     var token;
     var tokenBalance;
-    var expected = 100000000; // 100m
 
-    return Token.new('Farad', 'FRD', 8, expected).then(function(instance) {
+    return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
       token = instance;
       return token.balanceOf.call(accounts[0]);
     }).then( function(balance) {
       tokenBalance = balance.toNumber();
     }).then( function() {
-      assert.equal(tokenBalance, expected, "100m is expected");
+      assert.equal(tokenBalance, supply, "125m is expected");
     });
   });
 
   it('shall make a correct transfer', function() {
-    var expected = 100000000; // 100m
     var token;
-    var owner = accounts[0];
-    var dev1 = 0x47204B7AF24dab0fd2d8E65Ed679612Ee45AC40B;
 
-    return Token.new('Farad', 'FRD', 8, expected).then(function(instance) {
+    return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
       token = instance;
       // get balance first. should be 100m
       return token.balanceOf.call(owner);
     }).then(function(balance) {
       // balance should be 100m
       var actual = balance.toNumber();
-      assert.equal(actual, expected, 'Expected 100m balance');
+      assert.equal(actual, supply, 'Expected 125m balance');
     }).then(function() {
       // make a transfer
       return token.transfer(dev1, 1000, {from: owner});
     }).then(function(result) {
       // ok, transfer is successful!      
-      console.log('transfer result:', result);
+      // console.log('transfer result:', result);
       // get the balance
       return token.balanceOf.call(dev1);
-    }).then(function(dev1_bal) {
+    }).then(function(res) {
       // dev1 balance should be 1000
-      var actual = dev1_bal.toNumber();
-      assert.equal(actual, 1000, 'Expected balance of 1000');
+      assert.equal(res.toNumber(), 1000, 'Expected balance of 1000');
     }).then(function() {
       // get the acc0 balance
       return token.balanceOf.call(owner);
     }).then(function(m_bal) {
       // expect balance of account[0] to be 1000 less
-      var bal = expected - 1000;
+      var bal = supply - 1000;
       var actual = m_bal.toNumber();      
       assert.equal(actual, bal, 'Expected to be 1000 less from 100m');
     }).catch(function(e) {
@@ -70,42 +71,37 @@ contract('Token', function(accounts) {
   });
 
   it('shall allow transfer from acc0 to dev1', function() {
-    var supply = 100000000; // 100m
     var token;
-    var owner = accounts[0];
-    var dev1 = 0x47204B7AF24dab0fd2d8E65Ed679612Ee45AC40B;
 
     return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
       token = instance;
 
       // make a transfer
-      return token.transferFrom(owner, dev1, 1000);
-    }).then(function(result) {
-      console.log('transfer result:', result);
+      return token.transfer(dev1, 1000, {from: owner});
+    }).then(function(res) {
+      console.log('txHash:', res.receipt.transactionHash);
       // check the balance for dev1
       return token.balanceOf.call(dev1);
-    }).then(function(dev1_bal) {
-      var actual = dev1_bal.toNumber();
-      assert.equal(actual, 1000, 'Expected to be 1000');
+    }).then(function(res) {
+      assert.equal(res.toNumber(), 1000, 'Expected to be 1000');
     }).then(function() {
       // check balance of owner
       return token.balanceOf.call(owner);
-    }).then(function(owner_bal) {
-      var actual = owner_bal.toNumber();
-      assert.equal(actual, supply - 1000, 'Extected to be 1000 less');
+    }).then(function(res) {
+      assert.equal(res.toNumber(), supply - 1000, 'Extected to be 1000 less');
     });
   });
 
-  it('shall allow dev1 to claim money', function() {
-    var supply = 100000000; // 100m
-    var token;
-    var owner = accounts[0];
-    var dev1 = 0x47204B7AF24dab0fd2d8E65Ed679612Ee45AC40B;
+  // it('shall allow dev1 to claim money', function() {
+  //   var supply = 100000000; // 100m
+  //   var token;
+  //   var owner = accounts[0];
+  //   var dev1 = 0x47204B7AF24dab0fd2d8E65Ed679612Ee45AC40B;
 
-    return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
-      token = instance;
-    });
+  //   return Token.new('Farad', 'FRD', 8, supply).then(function(instance) {
+  //     token = instance;
+  //   });
 
-  });  
+  // });  
 
 });
