942f38662d42301b35c0d87c8c57ecc35d90bf9b
Da Few <1ofdafew@gmail.com>
2017-06-30 01:07:50 +0800

    Added FRD test for all ERC20 functions

--- test/test-frd-cryptoken.js
+++ test/test-frd-cryptoken.js
@@ -6,10 +6,10 @@ contract('FRD', function(accounts) {
   // contract variables;
   //
   var frd;  // FRD instance
-  var supply = 100000000; // 100m
-  var owner = accounts[0];
-  var dev1 = 0x47204B7AF24dab0fd2d8E65Ed679612Ee45AC40B;
-  var dev2 = 0xf969a1BCF4C36871fe5571b24e65f720f115DdC2;
+  const supply = 100000000; // 100m
+  const owner = accounts[0];
+  const dev1 = accounts[1]; 
+  const dev2 = accounts[2]; 
 
   it('shall create the FRD token with 100m supply', function() {
     return FRD.new(supply).then(function(instance) {
@@ -40,7 +40,7 @@ contract('FRD', function(accounts) {
   it('shall allow transfer of 500 from dev1 to dev2 correctly', function() {
     return FRD.new(supply).then(function(instance) {
       frd = instance;
-      return frd.transfer(dev1, 1000);
+      return frd.transfer(dev1, 1000, {from: owner});
     }).then(function(res) {
       console.log('txHash:', res.receipt.transactionHash);
       return frd.balanceOf.call(owner);
@@ -49,7 +49,10 @@ contract('FRD', function(accounts) {
       return frd.balanceOf.call(dev1);
     }).then(function(res) {
       assert.equal(1000, res.toNumber(), 'should be 1000');
-      return frd.transferFrom(dev1, dev2, 500);
+      // set the allowance first
+      return frd.approve(dev2, 500, {from: dev1});
+    }).then(function(res) {
+      return frd.transferFrom(dev1, dev2, 500, {from: dev2});
     }).then(function(res) {
       // should be successful.
       console.log('txHash:', res.receipt.transactionHash);
@@ -76,7 +79,7 @@ contract('FRD', function(accounts) {
       return frd.balanceOf.call(dev1);
     }).then(function(res) {
       assert.equal(1000, res.toNumber(), 'should be 1000');
-      return frd.transferFrom(dev1, dev2, 2000);
+      return frd.transfer(dev2, 2000, {from: dev1});
     }).then(function(res) {
       // should be failed, don't check for res, or error thrown
     }).catch(function(e) {
@@ -88,6 +91,60 @@ contract('FRD', function(accounts) {
     }).then(function(res) {
       assert.equal(1000, res.toNumber(), 'dev1 shall have 1000 back');
     });
+  });
+
+  it('shall allow dev2 to spend at most 100 from the approved value', function() {
+    return FRD.new(supply).then(function(instance) {
+      frd = instance;
+      return frd.transfer(dev1, 1000, {from: owner});
+    }).then(function(res) {
+      // should be successful.
+      // console.log('Transferred 1000 from', owner, 'to', dev1);
+      console.log('txHash:', res.receipt.transactionHash);
+      return frd.balanceOf.call(owner);
+    }).then(function(res) {
+      assert.equal(supply - 1000, res.toNumber(), 'should be 1000 less');
+      // console.log('dev1:', dev1);
+      return frd.balanceOf.call(dev1);
+    }).then(function(res) {
+      assert.equal(1000, res.toNumber(), 'should be 1000');
+      // console.log('dev1:', dev1);
+      // console.log('dev2:', dev2);
+      // console.log(dev1, 'approve', dev2, 'to spend at most 100');
+      return frd.approve(dev2, 100, {from: dev1});
+    }).then(function(res) {
+      // should be successful.
+      // console.log(dev1, 'approved 100 to', dev2)
+      console.log('txHash:', res.receipt.transactionHash);
+
+      // and allowable balance should be 100
+      return frd.allowance.call(dev1, dev2);
+    }).catch(function(e) {
+      console.log('Error in approving 100 to dev2:', e);
+      throw new Error(e);
+    }).then(function(res) {
+      // console.log('Allowance is', res.toNumber());
+      assert.equal(res.toNumber(), 100, 'shall be 100 in allowance');
+
+      // and dev2 can withdraw 50 out of it, can he?
+      // console.log('Calling transferFrom for', dev1, 'to', dev2);
+      return frd.transferFrom(dev1, dev2, 50, {from: dev2});
+    }).then(function(res) {
+      // should be successful.
+      // console.log(dev2, 'transferFrom', dev1, 'of 50')
+      console.log('txHash:', res.receipt.transactionHash);
+      // allowance balance shall be 50?
+      return frd.allowance.call(dev1, dev2);
+    }).then(function(res) {
+      assert.equal(res.toNumber(), 50, 'balance in allowance shall be 50');
+      // and balance is 50 as well
+      return frd.balanceOf.call(dev2);
+    }).then(function(res) {
+      assert.equal(res.toNumber(), 50, 'balanceOf shall be 50');
+    }).catch(function(e) {
+      console.log('Exiting test case:', e);
+    });
+
   });
 
 });
