65dcb3d8fb8f62691622d693d9aeeb8ace94c4ea
Da Few <1ofdafew@gmail.com>
2017-06-29 21:18:46 +0800

    changed from internal to public for testing

--- contracts/Operations.sol
+++ contracts/Operations.sol
@@ -7,19 +7,19 @@ contract Operations {
     // empty constructor
     function Operations() {}
 
-    function add(uint256 _x, uint256 _y) internal returns (uint256) {
+    function add(uint256 _x, uint256 _y) public returns (uint256) {
         uint256 z = _x + _y;
         assert(z >= _x);
         assert(z >= _y);
         return z;
     }
 
-    function subtract(uint256 _x, uint256 _y) internal returns (uint256) {
+    function subtract(uint256 _x, uint256 _y) public returns (uint256) {
         assert(_x >= _y);
         return _x - _y;
     }
 
-    function multiply(uint256 _x, uint256 _y) internal returns (uint256) {
+    function multiply(uint256 _x, uint256 _y) public returns (uint256) {
         uint256 z = (_x * _y);
         assert(_x == 0 ||  z/_x == _y);
         return z;
