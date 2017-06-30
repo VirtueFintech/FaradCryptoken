6eb0a89bad9bdbd90d70028dba12b38f16ea2f0a
Da Few <1ofdafew@gmail.com>
2017-06-30 23:06:42 +0800

    Updated for changes in the underlying Token contract

--- contracts/FRDCryptoken.sol
+++ contracts/FRDCryptoken.sol
@@ -3,16 +3,25 @@ pragma solidity ^0.4.11;
 
 import './Token.sol';
 
+/**
+ * FRDCryptoken is the main contract that will be published, including the
+ * manufacturing paramters that need to be pushed/published to the blockchain
+ * for traceability in the manufaturing process, as well as real-time updates
+ * on the escrow balance whenever the cryptoyalty is paid from the manufacturer.
+ *
+ */
 contract FRDCryptoken is Token {
 
-    string public name = 'FARAD';             // the token name
-    string public symbol = 'FRD';             // the token symbol
-    uint8 public decimals = 8;              // the number of decimals
+    string public name = 'FARAD';       // the token name
+    string public symbol = 'FRD';       // the token symbol
+    uint8 public decimals = 12;         // the number of decimals
 
     // our constructor, just supply the total supply.
-    function FRDCryptoken(
-        uint256 _totalSupply
-    ) Token(name, symbol, decimals, _totalSupply) {
+    function FRDCryptoken(uint256 _totalSupply) 
+        Token(name, symbol, decimals) 
+    {
+        totalSupply = _totalSupply;
+        balance[msg.sender] = _totalSupply;
     }
 
 }
